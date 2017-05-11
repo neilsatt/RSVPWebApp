f57091563fdc3073edba3f45bba5bbee329eaeed
Neil Satterfield <neilsatt@gmail.com>
2017-05-10 21:32:44 -0600

    updates to filter

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 // Select the form first, then the child
 const form = document.getElementById('registrar'); 
 const input = form.querySelector('input'); 
+
+const mainDiv = document.querySelector('.main');
 // Get reference to ul and add li items to it
 const ul = document.getElementById('invitedList'); // get ul
 
+// Filter out guests section
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckBox = document.createElement('input');
+
+
+
+// hide people who haven't responded
+filterLabel.textContent = "Hide those who haven't responded";
+filterCheckBox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckBox);
+mainDiv.insertBefore(div, ul);
+
+
+
 function createLI(text) {
     const li = document.createElement('li'); // create list item   
-    li.textContent = text;
+    // create an editable span
+    const span = document.createElement('span');
+    span.textContent = text;
+    li.appendChild(span);
+    
     // Label and Checkbox
     const label = document.createElement('label');
     label.textContent = 'Confirmed';
@@ -61,17 +83,34 @@ ul.addEventListener('click', (e) => {
         if (button.textContent === 'remove') {          
               ul.removeChild(li);
             }  else if (button.textContent === 'edit') {
-                
+                // create a new edit field and remove the span
+                const span = li.firstElementChild;
+                const input = document.createElement('input');
+                input.type = 'text';
+                input.value = span.textContent;
+                li.insertBefore(input, span);
+                li.removeChild(span);
+                // change edit button to a save button
+                button.textContent = 'save';
+            } 
+            // now reverse the process if it's a save button
+           else if (button.textContent === 'save') {
+                // create a new span
+                const input = li.firstElementChild;
+                const span = document.createElement('span');
+                span.textContent = input.value;
+                li.insertBefore(span, input);
+                li.removeChild(input);
+                // change save button to a edit button
+                button.textContent = 'edit';
             }
          
     }
 });
 
-/* Edit names 
-    - Add Edit button
-    - Make Text field editable
-    - Add Save button   
-*/
+
+
+
 
 
 
